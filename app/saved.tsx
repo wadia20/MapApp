f927d6a542eab718bfb227c082852562f5b8aae7
AHMED OUADIAA SOUIKI <159696190+wadia20@users.noreply.github.com>
2025-04-22 22:14:35 +0100

    Delete app directory

--- app/saved.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-const Page = () => {
-    return (
-        <View style ={{ flex: 1, justifyContent: 'center', alignItems:'center' }}>
-            <Text>saved places</Text>
-        </View>
-    );
-};
-export default Page;
